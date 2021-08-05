0b9d4d07fad55a256ac81bf2e46f52666483d676
nattensleplubo <nathan.gobert2002@gmail.com>
2021-08-05 13:53:43 +0200

    Test

--- libft.h
+++ libft.h
@@ -7,5 +7,9 @@
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_strlen(const char *str);
+int	ft_atoi(const char *str);
 
 #endif
