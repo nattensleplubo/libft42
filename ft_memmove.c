3431fe8572186f3cd8c315cea01faf3009295ce0
nattensleplubo <nathan.gobert2002@gmail.com>
2021-08-10 15:24:39 +0200

    Added headers

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,3 +1,12 @@
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+    int	i;
+
+	i = 0;
+	while (i < len)
+	{
+		
+	}
+}
