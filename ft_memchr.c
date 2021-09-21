ee44ee06db9dfc4650637382be22964596f16d4d
nattensleplubo <nathan.gobert2002@gmail.com>
2021-09-22 01:40:10 +0200

    jsais pu

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,13 +6,20 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/12 15:18:49 by ngobert           #+#    #+#             */
-/*   Updated: 2021/08/12 15:22:16 by ngobert          ###   ########.fr       */
+/*   Updated: 2021/09/21 17:28:37 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
+	int	i;
 	
+	i = 0;
+	while (i <= c)
+	{
+		if ((((unsigned char *)s) [i]) == (((unsigned char *)s)[c])
+		i++;
+	}
 }
