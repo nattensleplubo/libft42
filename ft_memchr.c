af765977e1d297718b872a31b639fd20732e6c4c
nattensleplubo <nathan.gobert2002@gmail.com>
2021-09-22 02:11:42 +0200

    lesgoooo g fini un truc en 3 mois

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/12 15:18:49 by ngobert           #+#    #+#             */
-/*   Updated: 2021/09/22 01:57:14 by ngobert          ###   ########.fr       */
+/*   Updated: 2021/09/22 02:09:02 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,13 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	{
 		while (!(((unsigned char *)s) [i]) == (((unsigned char *)s)[c])) // absolument deguelasse
 		{
-			if ((((unsigned char *)s) [i]) == (((unsigned char *)s)[c]))
+			if ((((unsigned char *)s) [i]) == (((unsigned char *)s)[c])) // wait c normé ca ? a verif sur vim
 			{
-				return(); // on return quoi meme ????
+				return ((void *)p + i);
 			}
 			i++;
 		}
 	}
+	return (NULL); 
 }
+// a verif jsp si c bon mais je pense que si
