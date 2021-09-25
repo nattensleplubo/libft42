652536b3e6d36a5450ccf5c9a7004f1582bdae96
nattensleplubo <nathan.gobert2002@gmail.com>
2021-09-25 13:41:49 +0200

    added ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,10 +6,27 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 13:10:38 by ngobert           #+#    #+#             */
-/*   Updated: 2021/09/25 13:20:25 by ngobert          ###   ########.fr       */
+/*   Updated: 2021/09/25 13:40:45 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (dst[i] && i < size)
+		i++;
+	while (src[j] && (i + j + 1) < size)
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	if (i < size)
+		dst[i + j] = 0;
+	return (i + ft_strlen(src));
+}
