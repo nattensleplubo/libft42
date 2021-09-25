f8e53ee0dbc0290c427e2270c99e2106f1ccc882
nattensleplubo <nathan.gobert2002@gmail.com>
2021-09-25 13:21:13 +0200

    Added few functions

---
+++ ft_memcmp.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/23 14:59:57 by ngobert           #+#    #+#             */
+/*   Updated: 2021/09/23 17:00:03 by ngobert          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+// La fonction memcmp() compare les n premiers octets des zones mémoire s1 et s2.
+// Elle renvoie un entier inférieur, égal, ou supérieur à zéro, si s1 est respectivement inférieure, égale ou supérieur à s2.   
+
+int memcmp(const void *s1, const void *s2, size_t n)
+{
+	
+}
