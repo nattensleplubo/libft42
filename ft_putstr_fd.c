b1bca996a4443955c1de321ef2cfd0b90bd8bce9
nattensleplubo <nathan.gobert2002@gmail.com>
2021-09-26 15:32:34 +0200

    Auto Commit

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 15:21:40 by ngobert           #+#    #+#             */
-/*   Updated: 2021/09/26 15:30:47 by ngobert          ###   ########.fr       */
+/*   Updated: 2021/09/26 15:32:24 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,4 @@ void ft_putstr_fd(char *s, int fd)
 		write(fd, s, 1);
 		s++;
 	}
-}
-
-int	main(void)
-{
-	ft_putstr_fd("Grosse queue toute jaune", 1);
 }
