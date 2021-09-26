d3324d79dcc4572dc171aec1609fa6034c80acd3
nattensleplubo <nathan.gobert2002@gmail.com>
2021-09-26 15:31:44 +0200

    Auto Commit

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,10 +6,22 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 15:21:40 by ngobert           #+#    #+#             */
-/*   Updated: 2021/09/26 15:23:18 by ngobert          ###   ########.fr       */
+/*   Updated: 2021/09/26 15:30:47 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putstr_fd(char *s, int fd);
+void ft_putstr_fd(char *s, int fd)
+{
+	while (*s)
+	{
+		write(fd, s, 1);
+		s++;
+	}
+}
+
+int	main(void)
+{
+	ft_putstr_fd("Grosse queue toute jaune", 1);
+}
