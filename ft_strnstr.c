22d402c7b922217225d3bde053b04e58376e6a0a
nattensleplubo <nathan.gobert2002@gmail.com>
2021-10-07 14:57:57 +0200

    Auto Commit

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,34 +6,34 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 14:51:30 by ngobert           #+#    #+#             */
-/*   Updated: 2021/10/07 14:53:49 by ngobert          ###   ########.fr       */
+/*   Updated: 2021/10/07 14:57:46 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strnstr(const char *haystack, const char *needle, size_t len);
+char	*strnstr(const char *haystack, const char *needle, size_t len)
 {
-	int	i;
-	int	j;
+	size_t	i;
+	size_t	j;
 
 	i = 0;
-	if (*to_find == '\0')
+	if (*needle== '\0')
 	{
-		return (str);
+		return (haystack);
 	}
-	while (str[i] != 0)
+	while (haystack[i] != 0)
 	{
 		j = 0;
-		while (to_find[j] == str[i + j])
+		while (needle[j] == haystack[i + j])
 		{
-			if (to_find[j + 1] == '\0')
+			if (needle[j + 1] == '\0')
 			{
-				return (str + i);
+				return (haystack + i);
 			}
 			j++;
 		}
 		i++;
 	}
-	return (0);
+	return (needle);
 }
