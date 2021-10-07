e18170f28bd089a103dda63777c5eb3113e23520
nattensleplubo <nathan.gobert2002@gmail.com>
2021-10-07 15:41:20 +0200

    Auto Commit

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,13 +6,13 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 14:51:30 by ngobert           #+#    #+#             */
-/*   Updated: 2021/10/07 15:24:00 by ngobert          ###   ########.fr       */
+/*   Updated: 2021/10/07 15:40:40 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	j;
@@ -26,12 +26,19 @@ char	*strnstr(const char *haystack, const char *needle, size_t len)
 		while (needle[j] == haystack[i + j])
 		{
 			if (needle[j + 1] == '\0')
-			{
 				return (haystack + i);
-			}
 			j++;
 		}
 		i++;
 	}
 	return (0);
+}
+
+int	main(void)
+{
+	const char	haystack[] = "Coca de piña";
+	const char	needle[] = "de";
+	size_t		len = 8;
+
+	printf("Vrai strnstr : %s\nFaux strnstr : %s", strnstr(haystack, needle, len), ft_strnstr(haystack, needle, len));
 }
