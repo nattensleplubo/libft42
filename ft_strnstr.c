9fb1b762d15c8aeac17bb88b80f5583409916e11
nattensleplubo <nathan.gobert2002@gmail.com>
2021-10-07 16:24:44 +0200

    Auto Commit

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,11 +6,12 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 14:51:30 by ngobert           #+#    #+#             */
-/*   Updated: 2021/10/07 15:40:40 by ngobert          ###   ########.fr       */
+/*   Updated: 2021/10/07 16:20:53 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
@@ -19,14 +20,14 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 
 	i = 0;
 	if (*needle == '\0')
-		return (haystack);
-	while (haystack[i] != 0 && i <= len)
+		return ((char *)haystack);
+	while ((char)haystack[i] != 0 && i <= len)
 	{
 		j = 0;
-		while (needle[j] == haystack[i + j])
+		while ((char)needle[j] == (char)haystack[i + j])
 		{
 			if (needle[j + 1] == '\0')
-				return (haystack + i);
+				return ((char *)haystack + i);
 			j++;
 		}
 		i++;
@@ -37,8 +38,8 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 int	main(void)
 {
 	const char	haystack[] = "Coca de piña";
-	const char	needle[] = "de";
-	size_t		len = 8;
+	const char	needle[] = "de ";
+	size_t		len = 15;
 
-	printf("Vrai strnstr : %s\nFaux strnstr : %s", strnstr(haystack, needle, len), ft_strnstr(haystack, needle, len));
+	printf("Vrai strnstr : %s\nFaux strnstr : %s\n", strnstr(haystack, needle, len), ft_strnstr(haystack, needle, len));
 }
