6e3187e83643eb1760c676911fb602e40dc1847a
nattensleplubo <nathan.gobert2002@gmail.com>
2021-10-10 18:43:08 +0200

    bite a cul

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,15 +6,11 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 01:41:13 by ngobert           #+#    #+#             */
-/*   Updated: 2021/10/06 18:53:18 by ngobert          ###   ########.fr       */
+/*   Updated: 2021/10/10 18:34:31 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-// Doit allouer la memoire necessaire pour un tableau de nmenmb elements de taille size_t octets et renvoie un pointeur vers la memoire allouee
-// La zone sera remplie avec des zeros ?? (donc des int ???)
-// Si nmemb ou size vaut zero on renvoi soit NULL, soit un pointeur unique??? (wtf???????????)
 
 void	*ft_calloc(size_t count, size_t size)
 {
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 21:57:00 by ngobert           #+#    #+#             */
-/*   Updated: 2021/10/10 18:30:28 by ngobert          ###   ########.fr       */
+/*   Updated: 2021/10/10 18:42:12 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,9 @@ static int	skip_beg(char const *s1, char const *set)
 	int	i;
 
 	i = 0;
-	if (is_set(s1[i], set) == 1)
-	{
-		while (is_set(s1[i], set) == 1)
-			i++;
-		return (i);
-	}
-	return (0);
+	while (is_set(s1[i], set) == 1)
+		i++;
+	return (i);
 }
 
 static int	skip_end(char const *s1, char const *set)
@@ -47,16 +43,13 @@ static int	skip_end(char const *s1, char const *set)
 
 	i = ft_strlen(s1) - 1;
 	j = 0;
-	if (is_set(s1[i], set) == 1)
+
+	while (is_set(s1[i], set) == 1)
 	{
-		while (is_set(s1[i], set) == 1)
-		{
-			i--;
-			j++;
-		}
-		return (j);
+		i--;
+		j++;
 	}
-	return (0);
+	return (j);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
@@ -67,7 +60,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	
 	i = skip_beg(s1, set);
 	j = 0;
-	dest = malloc(ft_strlen(s1) - skip_beg(s1, set) + skip_end(s1, set) + 1);
+	dest = malloc(ft_strlen(s1) - i - skip_end(s1, set) + 1);
 	if (!dest)
 		return (NULL);
 	while ((size_t)i < ft_strlen(s1) - skip_end(s1, set))
@@ -80,16 +73,16 @@ char	*ft_strtrim(char const *s1, char const *set)
 	return (dest);
 }
 
-// int main(void)
-// {
-// 	char const	s1[] = "!!!failure>>>succes<<<failure!!!";
-// 	char const	set[] = "failure!";
+int main(void)
+{
+	char const	s1[] = "!!!failure>>>succes<<<failure!!!";
+	char const	set[] = "failure!";
 	
-// 	printf("Char len	: %zu\n", ft_strlen(s1));
-// 	printf("Set in char	: %d\n", skip_beg(s1, set) + skip_end(s1, set));
-// 	printf("Beginning skip	: %d\n", skip_beg(s1, set));
-// 	printf("End skip	: %d\n", skip_end(s1, set));
-// 	printf("Sets		: %s\n", set);
-// 	printf("Untrimmed	: %s\n", s1);
-// 	printf("Trimmed		: %s\n", ft_strtrim(s1, set));
-// }
+	printf("Char len	: %zu\n", ft_strlen(s1));
+	printf("Set in char	: %d\n", skip_beg(s1, set) + skip_end(s1, set));
+	printf("Beginning skip	: %d\n", skip_beg(s1, set));
+	printf("End skip	: %d\n", skip_end(s1, set));
+	printf("Sets		: %s\n", set);
+	printf("Untrimmed	: %s\n", s1);
+	printf("Trimmed		: %s\n", ft_strtrim(s1, set));
+}
