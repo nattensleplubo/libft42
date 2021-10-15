24a006e4c35499fb333b85594aa8045fa49acae9
nattensleplubo <nathan.gobert2002@gmail.com>
2021-10-15 15:33:56 +0200

    Auto commit

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,17 +6,27 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 13:56:15 by ngobert           #+#    #+#             */
-/*   Updated: 2021/10/12 14:00:54 by ngobert          ###   ########.fr       */
+/*   Updated: 2021/10/15 15:33:33 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// Applique la fonction ’f’ à chaque caractère de la chaine de caractères passée en argument pour créer une nouvelle chaine de caractères résultant des applications successives de ’f’.
+#include "libft.h"
 
-// #1. La chaine de caractères sur laquelle itérer
-// #2. La fonction à appliquer à chaque caractère.
-
-// Return : La chaine de caractères résultant des applications successives de f. Retourne NULL si l’allocation échoue.
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	str = ft_strdup(s);
+	if (!s)
+		return (NULL);
+	if (!str)
+		return (NULL);
+	while (str[i])
+	{
+		str[i] = (*f)(i, str[i]);
+		i++;
+	}
+	return (str);
 }
